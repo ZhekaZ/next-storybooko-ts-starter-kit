678f051ba0630c71f0edaacebc05aca482c49c0d
yzhuravel <yzhuravel@yzhuravels-MacBook-Pro.local>
2022-01-13 17:27:58 +0200

    first commit

---
+++ .storybook/main.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "stories": [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ],
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "webpack5"
+  },
+  "typescript": { "reactDocgen": false } // https://issuehunt.io/r/storybookjs/storybook/issues/15336
+}
